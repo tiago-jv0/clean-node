2445252579ceb9850d1e4c1f5c48e37e112fdcd4
Tiago José <tiago_jvo@outlook.com>
2020-08-07 15:34:42 -0300

    test: ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,8 +4,8 @@ export const MongoHelper = {
 
   client: null as MongoClient,
 
-  async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri, {
+  async connect (): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL as string, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
